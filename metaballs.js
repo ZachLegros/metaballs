613833911cf39871c0df19832ce20c41ea11d102
zachlegros <legroszach3921@gmail.com>
2020-04-10 23:02:47 -0400

    create arcTask

--- metaballs.js
+++ metaballs.js
@@ -2,8 +2,8 @@
 
 const width = 700;
 const height = 500;
-const minSpeed = 1;
-const maxSpeed = 3;
+const minSpeed = 2;
+const maxSpeed = 4;
 const entities = 8;
 const maxSize = 80;
 const minSize = 15;
@@ -11,9 +11,11 @@ let blobs = [];
 
 // vector definition
 const RandSpeedVec = function(r) {
-  const coef = (r / (6 * maxSpeed));
-  this.x = (Math.random() * (maxSpeed - minSpeed) + minSpeed) / coef;
-  this.y = (Math.random() * (maxSpeed - minSpeed) + minSpeed) / coef;
+  const coef = (r / (4 * maxSpeed));
+  const xSign = Math.random() < 0.5 ? -1 : 1;
+  const ySign = Math.random() < 0.5 ? -1 : 1;
+  this.x = xSign * (Math.random() * (maxSpeed - minSpeed) + minSpeed) / coef;
+  this.y = ySign * (Math.random() * (maxSpeed - minSpeed) + minSpeed) / coef;
 }
 
 // blob definition
@@ -43,17 +45,26 @@ const Blob = function(x, y, r) {
   };
 }
 
+// grid helper 
+function getNode(grid, x, y) {
+  if (typeof grid !== undefined && grid !== null) {
+    return grid[x][y];
+  } else {
+    throw new Error('Grid invalid!');
+  }
+}
+
 // grid definition
 const Grid = function() {
   this.cells = [];
   this.nodes = [];
 
   this.show = () => {
-    for (var i = 0; i < this.cells.length; i++) {
+    /*for (var i = 0; i < this.cells.length; i++) {
       this.cells[i].forEach(cell => {
         cell.show();
       });
-    }
+    }*/
     for (var j = 0; j < this.nodes.length; j++) {
       this.nodes[j].forEach(node => {
         node.show();
@@ -70,18 +81,71 @@ const Grid = function() {
   }
 }
 
-// cell deninition
+// helpers for Cell
+function getDirection(cornerPos, cellOrigin) {
+  let coeff = 0;
+  if (cornerPos > cellOrigin) {
+    coeff = -1;
+  } else {
+    coeff = 1;
+  }
+  return coeff;
+}
+
+// cell definition
 const Cell = function(x, y, res, corners) {
   this.x = x;
   this.y = y;
   this.res = res;
   this.corners = corners;
-  
+
   this.show = () => {
     noFill();
     stroke('blue');
-    strokeWeight(1);
-    square(this.x - (this.res / 2), this.y - (this.res / 2), this.res);
+    strokeWeight(0.5);
+    square(this.x, this.y, this.res);
+  }
+
+  this.getArcTask = () => {
+    // arcToDo = [[[first link],[second link]]]
+    let arcToDo = [[]];
+    let tasks = 0;
+    this.corners.forEach(corner => {
+      if (corner.active == false) {
+        let xCoeff = getDirection(corner.x, this.x);
+        let yCoeff = getDirection(corner.y, this.y);
+        let horizNeighbourX = corner.x + (xCoeff * this.x);
+        let horizNeighbour = getNode(grid, horizNeighbourX, corner.y);
+        let vertNeighbourY = corner.y + (yCoeff * this.y);
+        let vertNeighbour = getNode(grid, corner.x, vertNeighbourY);
+        if (arcToDo.length != tasks + 1) {
+          arcToDo.push([]);
+        }
+        if (horizNeighbour.active == true && vertNeighbour.active == true) {
+          arcToDo[tasks].push([corner, horizNeighbour]);
+          arcToDo[tasks].push([corner, vertNeighbour]);
+          tasks += 1;
+        } else {
+          if (horizNeighbour.active == true) {
+            arcToDo[tasks].push([corner, horizNeighbour]);
+          }
+          if (vertNeighbour.active == true) {
+            arcToDo[tasks].push([corner, vertNeighbour]);
+          }
+        }
+      }
+    });
+    return arcToDo;
+  }
+  
+  // drawing the cell's arcs
+  // arcToDo = [[[first link],[second link]]]
+  this.createArc = arcToDo => {
+    for (let task = 0; task<arcToDo.length; task++) {
+      for (let link = 0; link<arcToDo[task].length; link++) {
+        
+      }
+    }
   }
 }
 
@@ -93,7 +157,7 @@ const Node = function(x, y) {
   this.sum = 0;
 
   this.update = () => {
-   this.sum = 0;
+    this.sum = 0;
     blobs.forEach(blob => {
       this.sum = this.sum + (Math.pow(blob.r, 2) / (Math.pow(this.x - blob.x, 2) + Math.pow(this.y - blob.y, 2)))
     });
@@ -109,9 +173,9 @@ const Node = function(x, y) {
     if (this.active === true) {
       fill('red');
     } else {
-      fill('lime');
+      fill('blue');
     }
-    circle(this.x, this.y, 5);
+    circle(this.x, this.y, 3);
   }
 }
 
@@ -135,9 +199,10 @@ function createGrid(res) {
   for (var cellX = 0; cellX < nX; cellX++) {
     newGrid.cells.push([]);
     for (var cellY = 0; cellY < nY; cellY++) {
-      // get corners
+      // get corners [topLeft, topRight, bottomLeft, bottomRight]
       let corners = [newGrid.nodes[cellX][cellY], newGrid.nodes[cellX + 1][cellY], newGrid.nodes[cellX][cellY + 1], newGrid.nodes[cellX + 1][cellY + 1]];
-      newGrid.cells[cellX].push(new Cell((cellX * res) + res / 2, (cellY * res) + res / 2, res, corners));
+      // create cell
+      newGrid.cells[cellX].push(new Cell((cellX * res), (cellY * res), res, corners));
     }
   }
 
@@ -155,13 +220,11 @@ function createBlobs() {
 }
 
 // global grid
-let grid = createGrid(25);
+let grid = createGrid(50);
 
 function setup() {
   createCanvas(width, height);
   createBlobs();
-  grid.update();
-  test = grid.nodes[3][3].active;
 }
 
 function draw() {
