e02c330418f82ac37d5a4a0a88dfb471fd8e2377
zachlegros <legroszach3921@gmail.com>
2020-04-11 19:57:05 -0400

    changed to classes

--- metaballs.js
+++ metaballs.js
@@ -9,59 +9,61 @@ let blobs = [];
 const res = 5;
 
 // vector definition
-const RandSpeedVec = function(r) {
-  const coef = (r / (4 * maxSpeed));
-  const xSign = Math.random() < 0.5 ? -1 : 1;
-  const ySign = Math.random() < 0.5 ? -1 : 1;
-  this.x = xSign * (Math.random() * (maxSpeed - minSpeed) + minSpeed) / coef;
-  this.y = ySign * (Math.random() * (maxSpeed - minSpeed) + minSpeed) / coef;
+class RandSpeedVec {
+  constructor(r) {
+    const coef = (r / (4 * maxSpeed));
+    const xSign = Math.random() < 0.5 ? -1 : 1;
+    const ySign = Math.random() < 0.5 ? -1 : 1;
+    this.x = xSign * (Math.random() * (maxSpeed - minSpeed) + minSpeed) / coef;
+    this.y = ySign * (Math.random() * (maxSpeed - minSpeed) + minSpeed) / coef;
+  }
 }
 
 // blob definition
-const Blob = function(x, y, r) {
-  this.x = x;
-  this.y = y;
-  this.r = r;
-  this.vel = new RandSpeedVec(r);
-
-  this.show = () => {
-    noFill();
-    stroke('red');
-    strokeWeight(2);
-    ellipse(this.x, this.y, this.r * 2, this.r * 3);
+class Blob {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.vel = new RandSpeedVec(r);
+    this.show = () => {
+      noFill();
+      stroke('red');
+      strokeWeight(2);
+      ellipse(this.x, this.y, this.r * 2, this.r * 3);
+    };
+    this.update = () => {
+      this.x = this.x + this.vel.x;
+      this.y = this.y + this.vel.y;
+      if (this.x + this.r > width || this.x - this.r < 0) {
+        this.vel.x = this.vel.x * (-1);
+      }
+      if (this.y + this.r > height || this.y - this.r < 0) {
+        this.vel.y = this.vel.y * (-1);
+      }
+    };
   }
-
-  this.update = () => {
-    this.x = this.x + this.vel.x;
-    this.y = this.y + this.vel.y;
-
-    if (this.x + this.r + 20 > width || this.x - this.r - 20 < 0) {
-      this.vel.x = this.vel.x * (-1);
-    }
-    if (this.y + this.r + 20 > height || this.y - this.r - 20 < 0) {
-      this.vel.y = this.vel.y * (-1);
-    }
-  };
 }
 
 // grid definition
-const Grid = function() {
-  this.cells = [];
-  this.nodes = [];
-
-  this.update = () => {
-    for (var k = 0; k < this.nodes.length; k++) {
-      this.nodes[k].forEach(node => {
-        node.update();
-        //node.show();
-      });
-    }
-    for (var l = 0; l < this.cells.length; l++) {
-      this.cells[l].forEach(cell => {
-        cell.update();
-        //cell.show();
-      });
-    }
+class Grid {
+  constructor() {
+    this.cells = [];
+    this.nodes = [];
+    this.update = () => {
+      for (var k = 0; k < this.nodes.length; k++) {
+        this.nodes[k].forEach(node => {
+          node.update();
+          //node.show();
+        });
+      }
+      for (var l = 0; l < this.cells.length; l++) {
+        this.cells[l].forEach(cell => {
+          cell.update();
+          //cell.show();
+        });
+      }
+    };
   }
 }
 
@@ -76,9 +78,11 @@ function getDirection(cornerPos, cellOrigin) {
   return coeff;
 }
 
-const Point = function(x, y) {
-  this.x = x;
-  this.y = y;
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
 // helper 
@@ -91,127 +95,128 @@ function findClosestPointFromMe(me, pointArray) {
 }
 
 // cell definition
-const Cell = function(x, y, corners) {
-  this.x = x;
-  this.y = y;
-  this.corners = corners;
-
-  this.show = () => {
-    noFill();
-    stroke('blue');
-    strokeWeight(0.5);
-    square(this.x, this.y, res);
-  }
-
-  this.getArcTask = () => {
-    // arcToDo = [[[first link],[second link]]]
-    let arcToDo = [
-      []
-    ];
-    let tasks = 0;
-    let activeCount = 0;
-    this.corners.forEach(corner => {
-      if (corner.active == false) {
-        let horizNeighbourX = corner.x + (getDirection(corner.x, this.x) * res);
-        let horizNeighbour = getNode(horizNeighbourX / res, corner.y / res);
-        let vertNeighbourY = corner.y + (getDirection(corner.y, this.y) * res);
-        let vertNeighbour = getNode(corner.x / res, vertNeighbourY / res);
-        if (arcToDo.length != tasks + 1) {
-          arcToDo.push([]);
-        }
-        if (horizNeighbour.active == true && vertNeighbour.active == true) {
-          arcToDo[tasks].push([corner, horizNeighbour]);
-          arcToDo[tasks].push([corner, vertNeighbour]);
-          tasks += 1;
-        } else {
-          if (horizNeighbour.active == true) {
+class Cell {
+  constructor(x, y, corners) {
+    this.x = x;
+    this.y = y;
+    this.corners = corners;
+    this.show = () => {
+      noFill();
+      stroke('blue');
+      strokeWeight(0.5);
+      square(this.x, this.y, res);
+    };
+    this.getArcTask = () => {
+      // arcToDo = [[[first link],[second link]]]
+      let arcToDo = [
+        []
+      ];
+      let tasks = 0;
+      let activeCount = 0;
+      this.corners.forEach(corner => {
+        if (corner.active == false) {
+          let horizNeighbourX = corner.x + (getDirection(corner.x, this.x) * res);
+          let horizNeighbour = getNode(horizNeighbourX / res, corner.y / res);
+          let vertNeighbourY = corner.y + (getDirection(corner.y, this.y) * res);
+          let vertNeighbour = getNode(corner.x / res, vertNeighbourY / res);
+          if (arcToDo.length != tasks + 1) {
+            arcToDo.push([]);
+          }
+          if (horizNeighbour.active == true && vertNeighbour.active == true) {
             arcToDo[tasks].push([corner, horizNeighbour]);
-          }
-          if (vertNeighbour.active == true) {
             arcToDo[tasks].push([corner, vertNeighbour]);
+            tasks += 1;
+          }
+          else {
+            if (horizNeighbour.active == true) {
+              arcToDo[tasks].push([corner, horizNeighbour]);
+            }
+            if (vertNeighbour.active == true) {
+              arcToDo[tasks].push([corner, vertNeighbour]);
+            }
           }
         }
-      } else {
-        activeCount += 1;
-      }
-    });
-    return arcToDo;
-  }
-
-  // returns a Point object 
-  // link = [unactive cell, active cell]
-  this.createPointFromLink = link => {
-    let unactive = link[0];
-    let active = link[1];
-    let tempX = null;
-    let tempY = null;
-
-    if (link[0].x == link[1].x) {
-      tempX = link[0].x;
-    } else if (link[0].y == link[1].y) {
-      tempY = link[0].y;
-    }
-
-    if (tempY == null) {
-      tempY = unactive.y + ((active.y - unactive.y) * ((1 - unactive.sum) / (active.sum - unactive.sum)));
-    } else if (tempX == null) {
-      tempX = unactive.x + ((active.x - unactive.x) * ((1 - unactive.sum) / (active.sum - unactive.sum)));
-    }
-
-    return new Point(tempX, tempY);
-  }
-
-  // arcToDo = [[[first link],[second link]]]
-  this.update = () => {
-    let arcToDo = this.getArcTask();
-    let activeCorners = [];
-    this.corners.forEach(corner => {
-      if (corner.active == true) {
-        activeCorners.push(corner);
-      }
-    });
-    if (arcToDo[0].length != 0) {
-      for (let task = 0; task < arcToDo.length; task++) {
-        let points = [];
-        for (let link = 0; link < 2; link++) {
-          let point = this.createPointFromLink(arcToDo[task][link]);
-          points.push(point);
+        else {
+          activeCount += 1;
         }
-        stroke('red');
-        strokeWeight(1);
-        line(points[0].x, points[0].y, points[1].x, points[1].y);
+      });
+      return arcToDo;
+    };
+    // returns a Point object 
+    // link = [unactive cell, active cell]
+    this.createPointFromLink = link => {
+      let unactive = link[0];
+      let active = link[1];
+      let tempX = null;
+      let tempY = null;
+      if (link[0].x == link[1].x) {
+        tempX = link[0].x;
       }
-    }
+      else if (link[0].y == link[1].y) {
+        tempY = link[0].y;
+      }
+      if (tempY == null) {
+        tempY = unactive.y + ((active.y - unactive.y) * ((1 - unactive.sum) / (active.sum - unactive.sum)));
+      }
+      else if (tempX == null) {
+        tempX = unactive.x + ((active.x - unactive.x) * ((1 - unactive.sum) / (active.sum - unactive.sum)));
+      }
+      return new Point(tempX, tempY);
+    };
+    // arcToDo = [[[first link],[second link]]]
+    this.update = () => {
+      let arcToDo = this.getArcTask();
+      let activeCorners = [];
+      this.corners.forEach(corner => {
+        if (corner.active == true) {
+          activeCorners.push(corner);
+        }
+      });
+      if (arcToDo[0].length != 0) {
+        for (let task = 0; task < arcToDo.length; task++) {
+          let points = [];
+          for (let link = 0; link < 2; link++) {
+            let point = this.createPointFromLink(arcToDo[task][link]);
+            points.push(point);
+          }
+          stroke('red');
+          strokeWeight(1);
+          line(points[0].x, points[0].y, points[1].x, points[1].y);
+        }
+      }
+    };
   }
 }
 
 // node definition
-const Node = function(x, y) {
-  this.x = x;
-  this.y = y;
-  this.active = false;
-  this.sum = 0;
-
-  this.update = () => {
+class Node {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.active = false;
     this.sum = 0;
-    blobs.forEach(blob => {
-      this.sum = this.sum + (Math.pow(blob.r, 2) / (Math.pow(this.x - blob.x, 2) + Math.pow(this.y - blob.y, 2)))
-    });
-    if (this.sum >= 1) {
-      this.active = true;
-    } else {
-      this.active = false;
-    }
-  }
-
-  this.show = () => {
-    noStroke();
-    if (this.active === true) {
-      fill('red');
-    } else {
-      fill('blue');
-    }
-    circle(this.x, this.y, 3);
+    this.update = () => {
+      this.sum = 0;
+      blobs.forEach(blob => {
+        this.sum = this.sum + (Math.pow(blob.r, 2) / (Math.pow(this.x - blob.x, 2) + Math.pow(this.y - blob.y, 2)));
+      });
+      if (this.sum >= 1) {
+        this.active = true;
+      }
+      else {
+        this.active = false;
+      }
+    };
+    this.show = () => {
+      noStroke();
+      if (this.active === true) {
+        fill('red');
+      }
+      else {
+        fill('blue');
+      }
+      circle(this.x, this.y, 3);
+    };
   }
 }
 
